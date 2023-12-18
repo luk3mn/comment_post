ca6bb9478ef4ef4bb925cf3ad8dc9597e2113576
luk3mn <lucasrenan2015@gmail.com>
2023-12-18 16:58:58 -0300

    config: project created

---
+++ src/test/java/com/luke/commentpost/CommentPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.commentpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
